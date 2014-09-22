5099812cc2f61dc67e47048df623163d8d3aea76
W4118 Student <w4118@w4118>
2014-09-21 23:08:03 -0400

    add a simple test, runs OK, still lots of bugs i think

--- prinfo.c
+++ prinfo.c
@@ -1,3 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "kernel/include/linux/prinfo.h"
+struct prinfo;
+
 int main(int argc, char **argv) {
+	struct prinfo *buf = malloc(sizeof(struct prinfo) * 500);
+	int nr = 500;
+	int i;
+	syscall(223, buf, &nr);
+	for (i = 0; i < nr; ++i) {
+		//printf(/* correct number of \t */);
+		struct prinfo p = buf[i];
+		printf("%s,%d,%ld,%d,%d,%d,%d\n", p.comm, p.pid, p.state,p.parent_pid, p.first_child_pid, p.next_sibling_pid, p.uid);
+	}
 	return 0;
 }
+
