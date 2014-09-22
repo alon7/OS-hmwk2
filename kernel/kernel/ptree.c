b92cfbe26efa8c18b61b8ef444d448c8d5015312
W4118 Student <w4118@w4118>
2014-09-21 22:16:26 -0400

    compile OK now, need testing

--- kernel/kernel/ptree.c
+++ kernel/kernel/ptree.c
@@ -1,3 +1,4 @@
+#include <linux/cred.h>
 #include <linux/unistd.h>
 #include <linux/sched.h>
 #include <linux/types.h>
@@ -21,6 +22,8 @@
  * };
  */
 
+void dfs_prinfo_copy(struct task_struct *head, struct prinfo *kernel_buf, int *copy_count, int *pr_count, int space_count);
+
 /**
  * sys_ptree - get the process tree
  * @buf: to store the processes tree
@@ -31,30 +34,37 @@ SYSCALL_DEFINE2(ptree, struct prinfo __user *, buf, int __user *, nr)
 	int copy_count = 0;
 	int pr_count = 0;
 	int space_count = 0;
-	int i;
+	struct task_struct *p;
+	struct prinfo *kernel_buf;
 	if (nr == NULL) {
-		return ERR_PTR(-EINVAL);
+		return -EINVAL;
 	} 
 	if (access_ok(VERIFY_WRITE, nr, sizeof(int)) == 0) {
-		return ERR_PTR(-EFAULT);
+		return -EFAULT;
 	}
-	copy_from_user(&space_count, nr, sizeof(int));
+	if (copy_from_user(&space_count, nr, sizeof(int)) != 0) {
+		return -EINVAL;
+	}
 	if (buf == NULL) {
-		return ERR_PTR(-EINVAL);
+		return -EINVAL;
 	} 
 	if (access_ok(VERIFY_WRITE, buf, space_count * sizeof(struct prinfo)) == 0) {
-		return ERR_PTR(-EFAULT);
+		return -EFAULT;
 	}
-	struct prinfo *kernel_buf = kmalloc(space_count * sizeof(struct prinfo), __GFP_WAIT);
+	kernel_buf = kmalloc(space_count * sizeof(struct prinfo), __GFP_WAIT);
 	read_lock(&tasklist_lock);
-	task_struct *p = init_task;
-	while(p->real_parent != NULL && p->real_parent.pid != 0 && p->real_parent != p) {
+	p = &init_task;
+	while(p->real_parent != NULL && p->real_parent->pid != 0 && p->real_parent != p) {
 		p = p->real_parent;
 	}
 	dfs_prinfo_copy(p, kernel_buf, &copy_count, &pr_count, space_count);	
 	read_unlock(&tasklist_lock);
-	copy_to_user(buf, kernel_buf, sizeof(struct prinfo) * copy_count);
-	copy_to_user(nr, &copy_count, sizeof(int));
+	if (copy_to_user(buf, kernel_buf, sizeof(struct prinfo) * copy_count) != 0) {
+		return -EINVAL;
+	}
+	if (copy_to_user(nr, &copy_count, sizeof(int)) != 0) {
+		return -EINVAL;
+	}	
 	kfree(kernel_buf);
 	return pr_count;
 }
@@ -63,7 +73,7 @@ void dfs_prinfo_copy(struct task_struct *head, struct prinfo *kernel_buf, int *c
 {
 	struct task_struct *p = head;
 	struct list_head *list;
-
+	int i;
 	(*pr_count)++;
 	if (*copy_count < space_count) {
 		kernel_buf[*copy_count].parent_pid = p->real_parent->pid;
@@ -71,23 +81,23 @@ void dfs_prinfo_copy(struct task_struct *head, struct prinfo *kernel_buf, int *c
 		if (p->children.next == &p->children)
 			kernel_buf[*copy_count].first_child_pid = 0;
 		else
-			kernel_buf[*copy_count].first_child_pid = list_entry(p->children.next, struct task_struct, sibling)->pid;
-		if (p->sibling == NULL || p->sibling.next == NULL || p->sibling.next == &p->sibling 
+			kernel_buf[*copy_count].first_child_pid = list_entry(p->children.next, struct task_struct, sibling)->pid;
+		if (p->sibling.next == NULL || p->sibling.next == &p->sibling 
 			|| p->sibling.next == &p->real_parent->children)
 			kernel_buf[*copy_count].next_sibling_pid = 0;
 		else
-			kernel_buf[*copy_count].next_sibling_pid = list_entry(p->children.next, struct task_struct, sibling)->pid;	
+			kernel_buf[*copy_count].next_sibling_pid = list_entry(p->children.next, struct task_struct, sibling)->pid;	
 		kernel_buf[*copy_count].state = p->state;
-		kernel_buf[*copy_count].uid = p->cred->uid_t;
+		kernel_buf[*copy_count].uid = p->cred->uid;
 		for (i = 0; i < 64; ++i) {
-			(kernel_buf[copy_count].comm)[i] = (p->comm)[i];
+			(kernel_buf[*copy_count].comm)[i] = (p->comm)[i];
 			if ((p->comm)[i] == '\0')
 				break;
 		}
 		(*copy_count)++;
 	}
 	list_for_each(list, &head->children) {
-		struct task_struct *p = list_entry(head, struct task_struct, sibling);
+		struct task_struct *p = list_entry(list, struct task_struct, sibling);
 		dfs_prinfo_copy(p, kernel_buf, copy_count, pr_count, space_count);	
 	}
 }
