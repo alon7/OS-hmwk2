85128a47d1311d69ea08eaee02696f5fe41dea2e
Qiming Chen <qc2195@columbia.edu>
2014-09-29 03:18:46 -0400

    pass checkpatch now

--- prinfo.c
+++ prinfo.c
@@ -9,25 +9,13 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include "kernel/include/linux/prinfo.h"
-struct prinfo;
-/*
-struct prinfo {
-	pid_t parent_pid;
-	pid_t pid;
-	pid_t first_child_pid;
-	pid_t next_sibling_pid;
-	long state;
-	long uid;
-	char comm[64];
-};
-*/
-typedef struct linknode {
+struct linknode {
 	struct prinfo task;
 	int top;
 	struct linknode *next;
 } LiStack;
 LiStack *s;
-void InitStack()
+void InitStack(void)
 {
 	s = (LiStack *)malloc(sizeof(LiStack));
 	s->top = -1;
@@ -36,15 +24,17 @@ void InitStack()
 void Push(struct prinfo element)
 {
 	LiStack *p;
+
 	p = (LiStack *)malloc(sizeof(LiStack));
 	p->task = element;
 	p->next = s->next;
 	s->next = p;
 	s->top++;
 }
-int Pop()
+int Pop(void)
 {
 	LiStack *p;
+
 	if (s->top == -1)
 		return 0;
 	p = s->next;
@@ -53,10 +43,11 @@ int Pop()
 	free(p);
 	return 1;
 }
-void ClearStack()
+void ClearStack(void)
 {
 	LiStack *p = s;
 	LiStack *q = s->next;
+
 	while (q != NULL) {
 		free(p);
 		p = q;
@@ -68,58 +59,13 @@ int main(int argc, const char *argv[])
 {
 	struct prinfo *buf;
 	int *nr;
+	int i, j;
+
 	nr = malloc(sizeof(int));
 	*nr = 500;
 	buf = (struct prinfo *)malloc(sizeof(struct prinfo) * 500);
-/*
-	buf->pid = 0;
-	buf->next_sibling_pid = 0;
-	buf->first_child_pid = 1;
-	(buf + 1)->comm = "init";
-	(buf + 1)->pid = 1;
-	(buf + 1)->next_sibling_pid = 2;
-	(buf + 1)->first_child_pid = 31;
-	(buf + 2)->comm = "ueventd";
-	(buf + 2)->pid = 31;
-	(buf + 2)->next_sibling_pid = 42;
-	(buf + 2)->first_child_pid = 0;
-	(buf + 3)->comm = "";
-	(buf + 3)->pid = 42;
-	(buf + 3)->next_sibling_pid = 43;
-	(buf + 3)->first_child_pid = 31;
-	(buf + 4)->pid = 7;
-	(buf + 4)->next_sibling_pid = 8;
-	(buf + 5)->pid = 2;
-	(buf + 5)->next_sibling_pid = 3;
-	(buf + 5)->first_child_pid = 8;
-	(buf + 6)->pid = 8;
-	(buf + 6)->next_sibling_pid = 9;
-	(buf + 6)->first_child_pid = 13;
-	(buf + 7)->pid = 13;
-	(buf + 7)->next_sibling_pid = 14;
-	(buf + 8)->pid = 14;
-	(buf + 8)->next_sibling_pid = 15;
-	(buf + 9)->pid = 9;
-	(buf + 9)->next_sibling_pid = 10;
-	(buf + 9)->first_child_pid = 15;
-	(buf + 10)->pid = 15;
-	(buf + 10)->next_sibling_pid = 0; //attention
-	(buf + 11)->pid = 3;
-	(buf + 11)->next_sibling_pid = 4;
-	(buf + 11)->first_child_pid = 10;
-	(buf + 12)->pid = 10;
-	(buf + 12)->next_sibling_pid = 11;
-	(buf + 13)->pid = 4;
-	(buf + 13)->next_sibling_pid = 0; //attention
-	(buf + 13)->first_child_pid = 11;
-	(buf + 14)->pid = 11;
-	(buf + 14)->next_sibling_pid = 12;
-	(buf + 15)->pid = 12;
-	(buf + 15)->next_sibling_pid = 0;
- */
 	syscall(223, buf, nr);
 	InitStack();
-	int i, j;
 	for (i = 0; i < *nr; i++) {
 		Push(*(buf+i));
 		for (j = 0; j < s->top; j++)
